3dea4217da56e6f5b2e288ba060bd4bf1e63a1a6
agus-darmawan <wayanagus.dr@gmail.com>
2025-05-28 18:00:07 +0700

    fix: error in db and add new env example

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,34 +1,56 @@
 import dotenv from "dotenv";
 
+// Load environment variables from .env file
 dotenv.config();
 
-const config = {
-  development: {
-    username: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME || "bookstore",
-    host: process.env.DB_HOST || "localhost",
-    port: Number(process.env.DB_PORT) || 5432,
-    dialect: "postgres",
-    logging: false,
+interface Config {
+  mongo: {
+    username: string;
+    password: string;
+    dbName: string;
+    uri: string;
+  };
+  server: {
+    port: number;
+  };
+  environment: string;
+  jwt: {
+    secret: string;
+    expiration: string;
+  };
+  refreshToken: {
+    secret: string;
+    expiration: string;
+  };
+}
+
+// Load configuration from environment variables
+const config: Config = {
+  mongo: {
+    username: process.env.MONGO_INITDB_ROOT_USERNAME || "your_mongo_username",
+    password: process.env.MONGO_INITDB_ROOT_PASSWORD || "your_mongo_password",
+    dbName: process.env.MONGO_DB_NAME || "your_db_name",
+    uri:
+      process.env.MONGO_URI ||
+      `mongodb://${
+        process.env.MONGO_INITDB_ROOT_USERNAME || "your_mongo_username"
+      }:${
+        process.env.MONGO_INITDB_ROOT_PASSWORD || "your_mongo_password"
+      }@localhost:27017/${
+        process.env.MONGO_DB_NAME || "your_db_name"
+      }?authSource=admin`,
   },
-  test: {
-    username: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME || "bookstore_test",
-    host: process.env.DB_HOST || "localhost",
-    port: Number(process.env.DB_PORT) || 5432,
-    dialect: "postgres",
-    logging: false,
+  server: {
+    port: Number(process.env.PORT) || 3003,
   },
-  production: {
-    username: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME || "bookstore_production",
-    host: process.env.DB_HOST || "localhost",
-    port: Number(process.env.DB_PORT) || 5432,
-    dialect: "postgres",
-    logging: false,
+  environment: process.env.NODE_ENV || "development",
+  jwt: {
+    secret: process.env.JWT_SECRET || "your_jwt_secret",
+    expiration: process.env.JWT_EXPIRATION || "1h",
+  },
+  refreshToken: {
+    secret: process.env.REFRESH_TOKEN_SECRET || "your_refresh_token_secret",
+    expiration: process.env.REFRESH_TOKEN_EXPIRATION || "30d",
   },
 };
 
